573fe5613bbd618493fd499981b0046f8f5360ab
Mark Lowel Montealto <marklowelmontealto@Marks-MacBook-Air.local>
2025-11-23 05:58:27 -0800

    feat(lib-button-ui): Add Button on Shared Library

--- apps/web/src/app/app.ts
+++ apps/web/src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
-
+import { Button } from '@gurokonekt/ui';
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [NxWelcome, RouterModule, Button],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
---
+++ lib/ui/src/lib/button/button.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gk-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.html',
+  styleUrl: './button.scss',
+})
+export class Button {}
