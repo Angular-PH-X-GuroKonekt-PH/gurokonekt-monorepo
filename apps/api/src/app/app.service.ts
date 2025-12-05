6b3e98d7a0dfcb9ecb2e2a9a8cbb90bd5c7794c4
Mark Lowel Montealto <lowel.montealto@whitecloak.com>
2025-12-05 16:43:30 +0800

    fix(ci): Test release

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Hello API TEST' };
+    return { message: 'Hello API TEST Release' };
   }
 }
