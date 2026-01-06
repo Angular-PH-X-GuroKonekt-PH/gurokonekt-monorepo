6f7ddff3677621c7b21d0299660d5bf9035d7719
Mark Lowel Montealto <lowel.montealto@whitecloak.com>
2026-01-06 18:21:24 +0800

    feat(landing-page): added footer

--- apps/landing-page/src/app/components/home/home.ts
+++ apps/landing-page/src/app/components/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from '../header/header';
 import { Creators } from '../creators/creators';
 import { Hero } from "../hero/hero";
+import { Footer } from "../footer/footer";
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Creators, Hero],
+  imports: [Header, Creators, Hero, Footer],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
