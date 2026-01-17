1c5b2e9eeb7551ab0220f4d2cdb4ecdaa70f5dd0
Andrew Oloroso <arw.oloroso@gmail.com>
2026-01-17 18:46:32 +0800

    refactor(constants): rename messages to constants

---
+++ lib/models/src/lib/constants.ts
@@ -0,0 +1,58 @@
+export const RETURN_MESSAGES = {
+  SUCCESS: {
+    SIGN_UP_SUCCESS: 'Sign up success',
+    SIGN_IN_SUCCESS: 'Sign in success',
+    EMAIL_SENT: 'Email sent',
+    EMAIL_UPDATED: 'Email updated',
+    PASSWORD_UPDATED: 'Password updated',
+    USER_AUTH_RETRIEVED: 'User auth retrieved',
+    SIGN_OUT_SUCCESS: 'Sign out success',
+    PROFILE_UPDATED: 'Profile updated',
+    ACCOUNT_DELETED: 'Account deleted',
+    PROFILE_CREATED: 'Profile created',
+    PROFILE_RETRIEVED: 'Profile retrieved',
+    USER_CREATED: 'User created',
+    USER_ROLE_UPDATED: 'User role updated',
+    USER_STATUS_UPDATED: 'User status updated',
+    GET_PROFILE: 'Profile retrieved',
+    FILE_UPLOAD_SUCCESS: 'File upload success',
+    FILE_DELETE_SUCCESS: 'File delete success',
+  },
+  FAILURE: {
+    SUPABASE_CREDENTIALS_NOT_FOUND: 'Supabase credentials not found',
+    INTERNAL_SERVER_ERROR: 'Internal Server Error',
+    SIGN_UP_FAILED: 'Sign up failed',
+    USER_NOT_FOUND: 'User not found',
+    ACCOUNT_ALREADY_DELETED: 'Account already deleted',
+    UNAUTHORIZED: 'Unauthorized',
+    PROFILE_NOT_FOUND: 'Profile not found',
+    UNSUPPORTED_FILE_TYPE: 'Unsupported file type',
+    FILE_URL_NOT_FOUND: 'File URL not found',
+    START_WITH_AVATAR: 'All storage paths must be avatar files (start with "avatars/")',
+    START_WITH_DOCUMENTS: 'All storage paths must be document files (start with "documents/")',
+  },
+  LINKS: {
+    DEFAULT_REDIRECT_URL: 'https://google.com',
+  }
+}
+
+export const BUCKET_NAME = 'attachments';
+
+export const DOCUMENTS_ALLOWED_TYPES = [
+  'application/pdf',
+  'application/msword',
+  'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  'application/vnd.ms-excel',
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+]
+
+export const AVATAR_ALLOWED_TYPES = [
+  'image/jpeg',
+  'image/jpg',
+  'image/png',
+  'image/gif',
+  'image/webp',
+  'image/svg+xml',
+  'image/bmp',
+  'image/tiff'
+]
