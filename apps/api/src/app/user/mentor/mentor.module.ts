11150c0f30f7198cc2cbfa0594da2a313a6cd3fd
Andrew Oloroso <arw.oloroso@gmail.com>
2026-01-17 20:05:16 +0800

    refactor(prisma): update prisma module and injection

--- apps/api/src/app/user/mentor/mentor.module.ts
+++ apps/api/src/app/user/mentor/mentor.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MentorService } from './mentor.service';
 import { MentorController } from './mentor.controller';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [MentorController],
   providers: [MentorService],
 })
