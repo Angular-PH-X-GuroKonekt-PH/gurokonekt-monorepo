e197c7326c57a7015ecff4efd30737cd8ef71d8e
felips_dulfz <felipinanerdulfo@gmail.com>
2026-02-13 01:12:04 +0800

    feat(landing-page): added contact us section and contact form

--- apps/landing-page/src/app/components/home/home.ts
+++ apps/landing-page/src/app/components/home/home.ts
@@ -3,10 +3,11 @@ import { Header } from '../header/header';
 import { Creators } from '../creators/creators';
 import { Hero } from "../hero/hero";
 import { Footer } from "../footer/footer";
+import { ContactUs } from "../contact-us/contact-us";
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Creators, Hero, Footer],
+  imports: [Header, Creators, Hero, Footer, ContactUs],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
