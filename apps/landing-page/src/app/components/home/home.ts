b1ace3daaa37d94def1f29a2d5a0d151b6d52587
Mark Lowel Montealto <102800815+KuyaGit@users.noreply.github.com>
2026-02-15 22:00:14 +0800

    feat(landing page): About Us (#141)
    * feat(landing-page): Added About Us
    
    * feat(landing-page): make spaceship rotate and make it bigger
    
    ---------
    
    Co-authored-by: Mark Lowel Montealto

--- apps/landing-page/src/app/components/home/home.ts
+++ apps/landing-page/src/app/components/home/home.ts
@@ -4,10 +4,11 @@ import { Creators } from '../creators/creators';
 import { Hero } from "../hero/hero";
 import { Footer } from "../footer/footer";
 import { ContactUs } from "../contact-us/contact-us";
+import { AboutUs } from '../about/about-us';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Creators, Hero, Footer, ContactUs],
+  imports: [Header, Creators, Hero, Footer, ContactUs, AboutUs],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
