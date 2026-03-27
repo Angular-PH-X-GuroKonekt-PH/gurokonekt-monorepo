a9af97a5c559c2677b32c54f675a3f639ea76255
ChugxScript <arw.oloroso@gmail.com>
2026-03-27 17:56:46 +0800

    Revert "fix(be): update downgrading, booking, and availability"
    This reverts commit dc19a4798a34592f5a7e616934b4a813b2f8bfd0.

--- apps/api/prisma/migrations/20260327000000_add_session_duration_minutes/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "mentor_profiles" ADD COLUMN "session_duration_minutes" INTEGER NOT NULL DEFAULT 60;
--- apps/api/prisma/migrations/20260327050350_update_is_profile_complete/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ALTER COLUMN "is_profile_complete" SET DEFAULT false;
