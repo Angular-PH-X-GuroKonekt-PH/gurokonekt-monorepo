61fecb435e21671c974bb5f0f0af0c73af397297
Matthew <matthew.uy@changeplan.co>
2026-05-10 16:09:53 +0800

    refactor: fix gk selector prefix and extract NavItem to sidebar.types

--- apps/web/src/app/layouts/components/sidebars/sidebar/sidebar.component.ts
+++ apps/web/src/app/layouts/components/sidebars/sidebar/sidebar.component.ts
@@ -3,15 +3,10 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 import { Store } from '@ngxs/store';
 import { IconComponent } from '../../../../shared/components/icon/icon.component';
 import * as AuthActions from '../../../../core/auth/store/auth.actions';
-
-export interface NavItem {
-  label: string;
-  route: string;
-  icon: string;
-}
+import { NavItem } from './sidebar.types';
 
 @Component({
-  selector: 'app-sidebar',
+  selector: 'gk-sidebar',
   imports: [RouterLink, RouterLinkActive, IconComponent],
   templateUrl: './sidebar.component.html',
 })
