c306ee3f6223e521e002d99c14662c05eeb84747
Seiji Ralph Villafranca <seiji.ralphvillafranca+kpn@kpn.com>
2026-05-10 08:35:59 +0200

    chore: change url for prod - change url for prod

--- apps/web/src/environments/environment.prod.ts
+++ apps/web/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-api.com/api',
+  apiUrl: 'https://api.gurokonekt.com/api',
   appName: 'GuroKonekt',
   version: '1.0.0',
 };
