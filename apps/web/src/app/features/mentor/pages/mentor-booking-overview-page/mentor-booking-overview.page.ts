52c157c5a86c8519646e319cbb2c3c4fee90ad84
Felipin Dulfo <felipinanerdulfo@gmail.com>
2026-05-22 19:10:43 +0800

    feat(fe): mentor dashboard my bookings (#235)

--- apps/web/src/app/features/mentor/pages/mentor-booking-overview-page/mentor-booking-overview.page.ts
+++ apps/web/src/app/features/mentor/pages/mentor-booking-overview-page/mentor-booking-overview.page.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MentorBookingService } from '../../services/mentor-booking.service';
+import { MentorBookingsTable } from "../../components/mentor-bookings-table/mentor-bookings-table";
+import { SectionTitle } from "../../../../../../../web/src/app/shared/components/section-title/section-title.component";
 
 @Component({
   selector: 'app-mentor-booking-overview-page',
-  imports: [],
+  imports: [MentorBookingsTable, SectionTitle],
   templateUrl: './mentor-booking-overview.page.html',
   styleUrl: './mentor-booking-overview.page.scss',
 })
-export class MentorBookingOverviewPage {}
+export class MentorBookingOverviewPage {
+    mentorBookings = inject(MentorBookingService);
+}
