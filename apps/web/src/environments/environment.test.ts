47c72fb8f3538c744267e226a31eba1079a38ef8
Joy Dimaculangan <joyreyesdimaculangan@gmail.com>
2026-08-09 04:41:25 +0800

    fix: redirect terms and conditions

--- apps/web/src/environments/environment.test.ts
+++ apps/web/src/environments/environment.test.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'https://test-api.gurokonekt.com/api',
+  landingPageUrl: 'https://gurokonekt.com',
   appName: 'GuroKonekt',
   version: '1.0.0',
 };
