26ce0335fb2db8481a354b7f00b326242c660481
Andrew Oloroso <101156843+ChugxScript@users.noreply.github.com>
2026-08-14 14:19:48 +0800

    chore(landing-page): configure the recaptcha site key (#373)

--- apps/landing-page/src/environments/environment.ts
+++ apps/landing-page/src/environments/environment.ts
@@ -7,5 +7,5 @@ export const environment: EnvironmentModel = {
     SIGN_IN_URL: 'https://test-portal.gurokonekt.com/login',
     REGISTER_URL: 'https://test-portal.gurokonekt.com/login',
     API_URL: 'https://test-api.gurokonekt.com/api',
-    RECAPTCHA_SITE_KEY: '',
+    RECAPTCHA_SITE_KEY: '6LcyYXstAAAAADjPzli4ry2bgHPFb9KG3MTpoDuF',
 };
